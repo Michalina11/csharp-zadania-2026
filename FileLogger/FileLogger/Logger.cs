9aa9db6030ec933f771a825c3539b4ac3668697e
Michalina11 <slomczynska.michalina@zst.radom.pl>
2026-05-13 18:38:20 +0200

    Add files via upload

---
+++ FileLogger/FileLogger/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileLogger
+{
+    public class Logger
+    {
+        private string filePath = "log.txt";
+        public void Write(string message)
+        {
+            string logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}";
+
+            File.AppendAllText(filePath, logMessage + Environment.NewLine);
+        }
+
+        public List<string> Search(string keyword)
+        {
+            if (!File.Exists(filePath))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(filePath)
+                       .Where(line =>line.Contains(keyword))
+                       .ToList();
+        }
+
+        public void Clear()
+        {
+            File.WriteAllText(filePath, string.Empty);
+        }
+    }
+}
---
+++ FileLogger/FileLogger/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            while (true)
+            {
+                Console.WriteLine("\n=== Dziennik Zdarzeń ===");
+                Console.WriteLine("[1] Dodaj wpis");
+                Console.WriteLine("[2] Szukaj");
+                Console.WriteLine("[3] Wyczyść");
+                Console.WriteLine("[0] Wyjdź");
+
+                Console.Write("Wybierz opcję: ");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.Write("Podaj treśc wpisu: ");
+                        string message = Console.ReadLine();
+
+                        logger.Write(message);
+
+                        Console.WriteLine("Wpis dodany.");
+                        break;
+
+                    case "2":
+                        Console.Write("Podaj słowo kluczowe: ");
+                        string keyword = Console.ReadLine();
+
+                        List<string> results = logger.Search(keyword);
+
+                        Console.WriteLine("\nWyniki wyszukiwania:");
+
+                        if (results.Count == 0)
+                        {
+                            Console.WriteLine("Brak wyników.");
+                        }
+                        else
+                        {
+                            foreach (string line in results)
+                            {
+                                Console.WriteLine(line);
+                            }
+                        }
+
+                        break;
+
+                    case "3":
+                        logger.Clear();
+                        Console.WriteLine("Plik został wyczyszczony.");
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Koniec programu");
+                        return;
+
+                    default:
+                        Console.WriteLine("Niepoprawna opcja.");
+                        break;
+                }
+            }
+        }
+    }
+}
